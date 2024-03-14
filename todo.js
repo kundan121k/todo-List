4a32f189ce37e00eb597d6bd3b267c3934d31158
Kundan <kundankumarssm04@gmail.com>
2024-03-14 15:55:40 +0530

    First

---
+++ todo.js
@@ -0,0 +1,28 @@
+let todoList = [];
+
+function showTask () {
+
+    let task = '';
+    for(let i = 0 ; i<todoList.length ; i++) {
+
+        task +=  `<p>${todoList[i]}</p>`;
+    }
+    document.querySelector('.js-div').innerHTML = task;
+}
+
+function handleadd() {
+    
+    console.log('chaliye shuru karte hai');
+    const inputElement = document.querySelector('.js-input');
+    const input = inputElement.value;
+    todoList.push(input);
+    inputElement.value = '';
+    showTask();
+}
+function handleKeyDown(event) {
+    console.log(event);
+    console.log(event.key);
+    if(event.key === 'Enter') {
+        handleadd();
+    }
+}
